5314ff549054e57bf6bdd4e02774715c8a45eea6
Marinela Rajkovic <marinela.rajkovic29@gmail.com>
2025-11-19 18:49:56 +0100

    Implement lead storage

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
+let myLeads = [ ]
 
-let myLeads = [  ]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+
     inputBtn.addEventListener("click", function() {
-
         myLeads.push(inputEl.value)
-
-            renderleads()
+            inputEl.value = ""
+            localStorage.setItem("myLeads", JSON.stringify(myLeads))
+                renderLeads()
+            console.log( localStorage.getItem("myLeads") )
     })
 
 
-function renderleads() {
-    let listItems = ""
-        for ( let i = 0; i < myLeads.length; i++ ) {
+    function renderLeads() {
+        let listItems = ""
+            for ( let i = 0; i < myLeads.length; i++ ) {
+                listItems +=  ` 
+                    <li> 
+                        <a target='_blank' href='${myLeads[i]}'>  
+                            ${myLeads[i]} 
+                        </a>
+                    </li> 
+                `
+            }
 
-            listItems +=  " <li> " + myLeads[i] + " </li> "
-
+            ulEl.innerHTML = listItems
         }
-        ulEl.innerHTML = listItems
-    }
